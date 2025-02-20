52ad8fc8fa22c6ed952bd4f13744e6e0be21d98f
Kelechi Okorie <kelechiokorie22@gmail.com>
2025-02-20 20:09:59 +0100

    Added connectStore and zustand

---
+++ store/connectedStore.js
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+
+export const useConnectedStore = create()(
+    persist(
+      (set, get) => ({
+        connected: false,
+        setConnected: () => set({ connected: true }),
+        setDisconnected: () => set({ connected: false }),
+      }),
+      {
+        name: 'connected-storage', // name of the item in the storage (must be unique)
+        storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+      },
+    ),
+  )
